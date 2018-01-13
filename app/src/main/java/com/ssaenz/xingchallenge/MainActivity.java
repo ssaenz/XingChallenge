5e79337e633f140254b95c486091b5c9c784ebc3
Sergio Sáenz <ssaenz@indaba.es>
2018-01-13 19:22:08 +0100

    Will use the RecyclerView to show the data. -> Added dependency. -> Added the xml definition and the property to MainActivity

--- app/src/main/java/com/ssaenz/xingchallenge/MainActivity.java
+++ app/src/main/java/com/ssaenz/xingchallenge/MainActivity.java
@@ -2,12 +2,21 @@ package com.ssaenz.xingchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadLaoutViews();
+    }
+
+    private void loadLaoutViews() {
+        mRecyclerView = findViewById(R.id.rv_list);
     }
 }
